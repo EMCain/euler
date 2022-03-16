9c14b4d7ea9ae50296d5b29890da9cb570d1ee04
EMCain <>
2022-03-15 19:59:56 -0700

    Add diagonal down + diag_set methods

--- 011_grid_product.py
+++ 011_grid_product.py
@@ -1,5 +1,6 @@
 from shared.grid import Grid
 
 test_grid = Grid("data_files/test_number_grid.txt")
-print(list(test_grid.get_n_right(2)))
-print(list(test_grid.get_n_diag_down(2)))
+print(list(test_grid.get_n_right(3)))
+print(list(test_grid.get_n_diag_down(3)))
+print(list(test_grid.get_n_diag_up(3)))
--- shared/grid.py
+++ shared/grid.py
@@ -1,24 +1,30 @@
-from xmlrpc.client import Boolean, boolean
-
-
 class Grid:
     rows = []
+    number_of_columns = 0
+
     def __init__(self, filepath): 
         print("Hi")
         with open(filepath, "r") as f:
             for row in f.readlines():
                 row_data = row.strip().split(" ")
+                if len(row_data) > self.number_of_columns:
+                    # This doesn't really account for rows of varying length
+                    # if we ever have to deal with them, it would be good to add some Null values to the end of shorter rows.
+                    self.number_of_columns == len(row_data)
                 self.rows.append([int(num) for num in row_data])
 
     
-    def diag_set(self, row: int, col: int, n: int, down: Boolean):
+    def diag_set(self, row: int, col: int, n: int, down: bool):
         for i in range(0, n):
-            c = col + i
-            if down: 
-                r = row + i 
-            else: 
-                r = row - i
-            yield self.rows[r][c]
+            try:
+                c = col + i
+                if down: 
+                    r = row + i 
+                else: 
+                    r = row - i
+                yield self.rows[r][c]
+            except IndexError:
+                raise ValueError(f"Row {c}, Col {c} out of range (overall grid size {len(self.number_of_columns)} x {len(self.rows)})")
 
 
     def get_n_right(self, n: int):
@@ -37,6 +43,15 @@ class Grid:
             row = self.rows[r]
             for c in range(0, len(row) - offset):
                 yield tuple(self.diag_set(r, c, n, True))
+
+    def get_n_diag_up(self, n: int):
+        if n < 1:
+            raise ValueError("n must be at least 1")
+        offset = n - 1
+        for r in range(offset, len(self.rows)):
+            row = self.rows[r]
+            for c in range(0, len(row) - offset):
+                yield tuple(self.diag_set(r, c, n, False))
 
 
     # next: implement similar methods for down, diagonal up, diagonal down 
